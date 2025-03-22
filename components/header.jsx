9d33ed52492e70ca09beda2fb688795ecaab4f23
Shubham Prakash <prakashshubham26@gmail.com>
2025-03-23 02:05:09 +0530

    Added Database using Prisma

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,12 @@
-"use client"; 
-
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 import Image from "next/image";
 import Link from 'next/link';
 import { Button } from './ui/button';
 import { LayoutDashboard, PenBox } from 'lucide-react';
-const header = () => {
+import { checkUser } from '@/lib/checkUser';
+
+const header = async () => {
+  await checkUser();
   return (
     <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
       <nav className='container mx-auto px-4 py-4 flex item-center justify-between'>
