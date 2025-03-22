44d4f55e9ab15621def68bb540343b520c82b315
Shubham Prakash <prakashshubham26@gmail.com>
2025-03-23 01:19:20 +0530

    Landing Page

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,21 +3,44 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Image from "next/image"
+import { useRef, useEffect } from "react";
 
 const HeroSection = () => {
+
+    const imageRef = useRef(null);
+    useEffect(() => {
+        const imageElement = imageRef.current;
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+    
+            if (scrollPosition > scrollThreshold) {
+                imageElement.classList.add("scrolled");
+            } else{
+                imageElement.classList.remove("scrolled");
+            }
+        };
+        window.addEventListener("scroll", handleScroll )
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, [])
+
   return (
     <div className="pb-20 px-4">
         <div className="container mx-auto text-center">
             <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title ">
                 Manage you finance <br /> with Intelligence
             </h1>
-            <p>
+            <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
                 An AI powered finance management tool that helps you track,
                 analyze and optimize your spending with real-time insights.
             </p>
-            <div>
+            <div className="flex justify-center space-x-4">
                 <Link href="/dashboard">
-                    <Button size="lg" className="px-8">
+                    <Button size="lg" className="px-8 bg-black text-white hover:bg-gray-800 "> 
                         Get Started
                     </Button>
                 </Link>
@@ -27,8 +50,8 @@ const HeroSection = () => {
                     </Button>
                 </Link>
             </div>
-            <div>
-                <div>
+            <div className="hero-image-wrapper">
+                <div ref={imageRef} className="hero-image">
                     <Image src="/banner.jpeg" 
                         width={1280} 
                         height={720} 
